02660aeac052b51b94309f0861be55a2e943e7d1
Anthonny <heatdkwow@gmail.com>
2024-02-01 15:31:51 -0500

    a

--- resources/views/paquetes/paquetes.blade.php
+++ resources/views/paquetes/paquetes.blade.php
@@ -14,21 +14,21 @@
                 <form method="POST" action = "{{ route('paquetes.store') }}">
                     @csrf
                     <input type="text" name="message"
-                    class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                    placeholder="{{ __('Put your message here') }}" value="{{ old('message') }}">
-                 <p>Nombre del paquete:</p>
-                <input type="text" name="nombre_paquete"
-                    class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                    placeholder="{{ __('Put your message here') }}" value="{{ old('message') }}">
-                <p>Numero de días: </p>
-                <input type="number" name="num_dias"
-                    class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                    placeholder="{{ __('Put your message here') }}" value="{{ old('num_dias') }}">
-                Número de noches: 
-                <input type="number" name="num_noches"
-                    class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                    placeholder="{{ __('Put your message here') }}" value="{{ old('num_noches') }}">
-                
+                        class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
+                        placeholder="{{ __('Put your message here') }}" value="{{ old('message') }}">
+
+                    <input type="text" name="nombre_paquete"
+                        class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
+                        placeholder="{{ __('Put your message here') }}" value="{{ old('message') }}">
+
+                    <input type="number" name="num_dias"
+                        class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
+                        placeholder="{{ __('Put your message here') }}" value="{{ old('num_dias') }}">
+
+                    <input type="number" name="num_noches"
+                        class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
+                        placeholder="{{ __('Put your message here') }}" value="{{ old('num_noches') }}">
+
                     <x-input-error :messages="$errors->get('message')" />
                     <x-primary-button class='mt-4'>Agregar</x-primary-button>
 
