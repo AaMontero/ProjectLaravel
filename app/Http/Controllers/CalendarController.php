acf5678148eff08206b3e0dbe8f283e16d7d1ce8
Anthonny <heatdkwow@gmail.com>
2024-02-07 11:49:24 -0500

    comentario

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,16 +10,38 @@ class CalendarController extends Controller
     //
     public function calendario()
     {
+        // $eventosManual = [
+        //     [
+        //         'title' => 'event1',
+        //         'start' => '2010-01-01'
+        //     ],
+        //     [
+        //         'title' => 'Reservado',
+        //         'start' => '2024-02-07',
+        //         'end' => '2024-02-15'
+        //     ],
+        //     [
+        //         'title' => 'Reservado',
+        //         'start' => '2024-02-07',
+        //         'end' => '2024-02-08'
+        //     ],
+        // ];
         $events = array();
         $eventos = Evento::all();
-        foreach($eventos as $eventos){
+        $estadoCalendario = "" ; 
+        foreach($eventos as $evento){
+            if( ($evento->estado) === "reservado"){
+                $estadoCalendario = "Esta reservado"; 
+            }
             $events[] = [
-                'titular'=> $eventos->titular,
-                'fecha_inicio'=>$eventos->fecha_inicio,
-                'fecha_salida'=>$eventos->fecha_salida,
-                'estado'=>$eventos->estado,
-                'descripcion'=>$eventos->descripcion,
-
+                // 'titular'=> $evento->titular,
+                // 'fecha_inicio'=>$evento->fecha_inicio,
+                // 'fecha_salida'=>$evento->fecha_salida,
+                // 'estado'=>$evento->estado,
+                // 'descripcion'=>$evento->descripcion,
+                'title' => $estadoCalendario,
+                'start' => $evento->fecha_inicio,
+                'end' => $evento->fecha_salida
             ];
         }
 
