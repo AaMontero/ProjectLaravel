596877db14f580a780cfcaff6c8439b263947d15
Joho06 <jhjaviersartica@gmail.com>
2024-02-21 12:35:48 -0500

    tabla de contratos css

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -90,7 +90,7 @@
     </div>
     <div class="py-8 relative z-50">
         <div class="max-w-7xl mx-auto px-2 lg:px-20 mb-4">
-            <div class="bg-white dark:bg-gray-900 bg-opacity-50  shadow-lg rounded-lg">
+            <div class="bg-white dark:bg-gray-900 bg-opacity-50 shadow-lg rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <table class="w-full bg-white dark:bg-gray-800 border border-gray-300 ">
                         <thead>
