da86d25fd1670b0a4ad1216a4dc11747ba42cb4d
Joho06 <jhjaviersartica@gmail.com>
2024-02-21 12:00:06 -0500

    tabla de contratos css

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -88,65 +88,70 @@
             }
         </script>
     </div>
-
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto px-4 lg:px-8 mb-4">
+            <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-lg rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table class=" w-100 mx-auto  bg-white border border-gray-300">
+                        <thead>
+                            <tr>
+                                <th class="py-2 px-4 border-b">Cédula</th>
+                                <th class="py-2 px-4 border-b">Nombres</th>
+                                <th class="py-2 px-4 border-b">Apellidos</th>
+                                <th class="py-2 px-4 border-b">Teléfono</th>
+                                <th class="py-2 px-4 border-b">Email</th>
+                                <th class="py-2 px-4 border-b">Provincia</th>
+                                <th class="py-2 px-4 border-b">Ciudad</th>
+                                <th class="py-2 px-4 border-b">Estado</th>
+                                <th class="py-2 px-4 border-b">Contrato</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($clientes as $cliente)
+                                <tr>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->cedula }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->nombres }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->apellidos }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->numTelefonico }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->email }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->provincia }}</td>
+                                    <td class="py-2 px-4 border-b text-center">{{ $cliente->ciudad }}</td>
+                                    <td class="py-2 px-4 border-b text-center">
+                                        @if ($cliente->activo == 1)
+                                            Activo
+                                        @else
+                                            Inactivo
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <x-dropdown>
+                                            <x-slot name="trigger">
+                                                <button>
+                                                    <svg class="ml-5 w-5 h-5 text-gray-400 dark:text-gray-200"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        stroke-width="1.5" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                                    </svg>
+                                                </button>
+                                            </x-slot>
+                                            <x-slot name="content">
+                                                <x-dropdown-link :href="route('clientes.edit', $cliente)">
+                                                    {{ __('Editar Cliente') }}
+                                                </x-dropdown-link>
+                                            </x-slot>
+                                        </x-dropdown>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</div>
 
     <!-- Tabla para visualizar los usuarios -->
-    <div class="mx-auto min-w-full">
 
-        <table class=" w-100 mx-auto  bg-white border border-gray-300">
-            <thead>
-                <tr>
-                    <th class="py-2 px-4 border-b">Cédula</th>
-                    <th class="py-2 px-4 border-b">Nombres</th>
-                    <th class="py-2 px-4 border-b">Apellidos</th>
-                    <th class="py-2 px-4 border-b">Número Telefónico</th>
-                    <th class="py-2 px-4 border-b">Email</th>
-                    <th class="py-2 px-4 border-b">Provincia</th>
-                    <th class="py-2 px-4 border-b">Ciudad</th>
-                    <th class="py-2 px-4 border-b">Estado</th>
-                    <th class="py-2 px-4 border-b">Contrato</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($clientes as $cliente)
-                    <tr>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->cedula }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->nombres }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->apellidos }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->numTelefonico }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->email }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->provincia }}</td>
-                        <td class="py-2 px-4 border-b text-center">{{ $cliente->ciudad }}</td>
-                        <td class="py-2 px-4 border-b text-center">
-                            @if ($cliente->activo == 1)
-                                Activo
-                            @else
-                                Inactivo
-                            @endif
-                        </td>
-                        <td>
-                            <x-dropdown>
-                                <x-slot name="trigger">
-                                    <button>
-                                        <svg class="ml-5 w-5 h-5 text-gray-400 dark:text-gray-200"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                        </svg>
-                                    </button>
-                                </x-slot>
-                                <x-slot name="content">
-                                    <x-dropdown-link :href="route('clientes.edit', $cliente)">
-                                        {{ __('Editar Cliente') }}
-                                    </x-dropdown-link>
-                                </x-slot>
-                            </x-dropdown>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
     @include('layouts.footer')
 </x-app-layout>
